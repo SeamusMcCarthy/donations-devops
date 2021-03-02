cbb9b9a5044cd69065fdc0029ba50561bb175974
Seamus McCarthy <47305550+spoderman01@users.noreply.github.com>
2021-03-02 00:01:45 +0000

    Changed case of PORT

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Handlebars = require("handlebars");
 const Joi = require("@hapi/joi");
 
 const server = Hapi.server({
-  port: process.env.port || 3000,
+  port: process.env.PORT || 3000,
 });
 
 async function init() {
