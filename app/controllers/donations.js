a2728cb80a75d94ea4e7ef7a70b2b9f85016294d
Seamus McCarthy <47305550+spoderman01@users.noreply.github.com>
2021-03-17 14:12:54 +0000

    Complete addition of candidate model and update report view

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -13,7 +13,7 @@ const Donations = {
   },
   report: {
     handler: async function (request, h) {
-      const donations = await Donation.find().populate("donor").lean();
+      const donations = await Donation.find().populate("donor").populate("candidate").lean();
       return h.view("report", {
         title: "Donations to Date",
         donations: donations,
@@ -26,10 +26,18 @@ const Donations = {
         const id = request.auth.credentials.id;
         const user = await User.findById(id);
         const data = request.payload;
+
+        const rawCandidate = request.payload.candidate.split(",");
+        const candidate = await Candidate.findOne({
+          lastName: rawCandidate[0],
+          firstName: rawCandidate[1],
+        });
+
         const newDonation = new Donation({
           amount: data.amount,
           method: data.method,
           donor: user._id,
+          candidate: candidate._id,
         });
         await newDonation.save();
         return h.redirect("/report");
